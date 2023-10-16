9fed226edf707084ddea265591ec429497021649
Abhimanyu Samantasinghar Samantaray <128180907+assamantaray95@users.noreply.github.com>
2023-10-16 17:45:04 +0530

    count digits which is divisible by original number
    Suppose you take 12 as a number
    your answer is = 2
    because in 12 both 1, and 2 are present and both are complete divisor of 12

---
+++ CountNoofDigitesDivisibleByOriginalNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class CountNoofDigitesDivisibleByOriginalNumber
+{
+    public static void Check(int n)
+    {
+        int counter = 0;
+        int number = n;
+        while(n>0)
+        {
+            int x = n%10;
+            if((number % x) == 0)
+            {
+                System.out.println("Your Given Number = "+number+" ,digits = "+x);
+        	    counter++;
+            }
+        	 n = n/10;
+        }
+        if(counter == 0){
+         System.out.println("There is no digits found in "+number+" which is divisibe by "+number);   
+        }
+        else
+        {
+         System.out.println(counter+" Digits of the Number "+number+" is divisible completely");
+         System.out.println("So Your answer is = "+counter);
+        }
+    }
+	public static void main(String[] args) {
+	    Scanner inputnum = new Scanner(System.in);
+	    System.out.println("Enter a Number To check how many digits are divisible by entered number");
+	    int n = inputnum.nextInt();
+	    Check(n);
+    }
+}
